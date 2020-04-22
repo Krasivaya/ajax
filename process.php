d730adee761864e0cd9d8bdfb0c6e53416ed01d7
Krasivaya <semwagacarine@gmail.com>
2020-04-22 17:45:16 +0200

    Check Post variable

--- process.php
+++ process.php
@@ -2,6 +2,11 @@
 
 echo "Processing...";
 
+// Check for POST Variable
+if(isset($_POST['name'])){
+  echo "POST: Your name is ". $_POST['name'];
+}
+
 // Check for GET Variable
 if(isset($_GET['name'])){
   echo "GET: Your name is ". $_GET['name'];
